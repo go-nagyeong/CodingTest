5cbb337fea9095f2c45fd556231cf01f56bde9d0
Gyeong <84227532+ngkim0929@users.noreply.github.com>
2021-06-24 17:06:11 +0900

    :hammer: Update development script

--- Programmers/약수의_개수와_덧셈.py
+++ Programmers/약수의_개수와_덧셈.py
@@ -11,3 +11,14 @@ def solution(left, right):
             answer -= i
         count = 0
     return answer
+
+# 더 좋은 방법
+def solution(left, right):
+    answer = 0
+    for i in range(left, right+1):
+        # 제곱근 값이 정수이면 약수의 개수가 홀수
+        if (i**0.5) == int(i**0.5):
+            answer -= i
+        else:
+            answer += i
+    return answer
