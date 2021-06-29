bd34d4360637530fd9962da968a3c9344c6570a2
Gyeong <84227532+ngkim0929@users.noreply.github.com>
2021-06-29 19:02:23 +0900

    :hammer: Update development scripts

--- Programmers/3진법_뒤집기.py
+++ Programmers/3진법_뒤집기.py
@@ -10,3 +10,11 @@ def solution(n):
     for i in range(len(ternary)):
         answer += ternary[i] * pow(3, i)
     return answer
+
+# 더 좋은 방법
+def solution(n):
+    rev = ''
+    while n > 0:
+        n, mod = divmod(n, 3)
+        rev += str(mod)
+    return int(rev, 3)
